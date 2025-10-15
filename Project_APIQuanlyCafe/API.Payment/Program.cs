a3e0fa2c137395731b3de678ea4a03ce4fb79f7b
Zwgn <dungb250905@gmail.com>
2025-10-15 21:27:38 +0700

    Update Program.cs

--- Project_APIQuanlyCafe/API.Payment/Program.cs
+++ Project_APIQuanlyCafe/API.Payment/Program.cs
@@ -1,17 +1,34 @@
+using Microsoft.OpenApi.Models;
+using DAL;
+using BLL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "QLyQuanCafe API", Version = "v1" });
+});
+
+// Register DatabaseHelper and BLL services
+builder.Services.AddScoped<DatabaseHelper>();
+builder.Services.AddScoped<BanDAL>();
+builder.Services.AddScoped<BanBLL>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Enable Swagger for both Development and when using IIS Express
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Development")
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "QLyQuanCafe API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseAuthorization();
