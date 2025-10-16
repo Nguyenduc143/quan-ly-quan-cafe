9bafbc221dcca1b1e9f334f8dd069c50476b9e06
Zwgn <dungb250905@gmail.com>
2025-10-16 20:24:54 +0700

    update progam.cs API.Products

--- Project_APIQuanlyCafe/API.Products/Program.cs
+++ Project_APIQuanlyCafe/API.Products/Program.cs
@@ -1,17 +1,25 @@
+﻿using DAL;
+using BLL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Đăng ký DatabaseHelper, ProductDAL, ProductBLL
+builder.Services.AddSingleton<DatabaseHelper>();
+builder.Services.AddScoped<ProductDAL>();
+builder.Services.AddScoped<ProductBLL>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
